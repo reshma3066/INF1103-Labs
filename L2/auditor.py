981353b0b01158e0869d6b3fb1174a49a3ef4b93
reshma3066 <2604005@sit.singaporetech.edu.sg>
2026-09-11 14:36:18 +0800

    Add input validation

--- L2/auditor.py
+++ L2/auditor.py
@@ -3,6 +3,12 @@ failed_entries = 0
 
 while True:
         stock = input("Enter stock quantity(or type 'quit' to exit): ")
-        if stock =='quit':
+        #is.lower() takes all inputs in lowercase
+        if stock.islower()  =='quit':
             break
+        
+        if not stock.isdigit():
+            print("Invalid input. Please enter a valid number.")
+            failed_entries += 1
+            continue
         
