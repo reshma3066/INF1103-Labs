6eb3cbdd2909031ec896d8cd352f9c015041eefe
reshma3066 <2604005@sit.singaporetech.edu.sg>
2026-09-11 16:39:46 +0800

    Update auditor and Dockerfile

--- L2/auditor.py
+++ L2/auditor.py
@@ -2,28 +2,23 @@ inventory = 0
 failed_entries = 0
 
 while True:
-        stock = input("Enter stock quantity(or type 'quit' to exit): ")
-        #is.lower() takes all inputs in lowercase
-        if stock =='quit':
-            break
-        
-        #validate non numeric input
-        #is.digit already rejects negative numbers
-        if not stock.isdigit():
-            print("Invalid input. Please enter a valid number.")
-            failed_entries += 1
-            continue
-        
-        #Convert input to integer
-        quantity = int(stock)
-        #Add to inventory
-        inventory += quantity
-        
-        if inventory > 500:
-            print("Overstock Alert! Inventory exceeds 500 units.")
-            break
-        
+    stock = input("Enter stock quantity (or type 'quit' to exit): ")
+
+    if stock == "quit":
+        break
+
+    if not stock.isdigit():
+        print("Invalid input. Please enter a valid number.")
+        failed_entries += 1
+        continue
+
+    quantity = int(stock)
+    inventory += quantity
+
+    if inventory > 500:
+        print("Overstock Alert! Inventory exceeds 500 units.")
+        break
+
 print("Total Units Processed:", inventory)
 print("Number of Failed/Rejected Entries:", failed_entries)
-        
         
