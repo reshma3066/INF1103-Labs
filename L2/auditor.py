44ca67e4c381e1840c746d4d4c4b06517b243121
reshma3066 <2604005@sit.singaporetech.edu.sg>
2026-09-11 14:43:02 +0800

    Add input tracking

--- L2/auditor.py
+++ L2/auditor.py
@@ -4,11 +4,20 @@ failed_entries = 0
 while True:
         stock = input("Enter stock quantity(or type 'quit' to exit): ")
         #is.lower() takes all inputs in lowercase
-        if stock.islower()  =='quit':
+        if stock =='quit':
             break
         
+        #validate non numeric input
+        #is.digit already rejects negative numbers
         if not stock.isdigit():
             print("Invalid input. Please enter a valid number.")
             failed_entries += 1
             continue
+        
+        #Convert input to integer
+        quantity = int(stock)
+        
+        #Add to inventory
+        inventory += quantity
+        
         
